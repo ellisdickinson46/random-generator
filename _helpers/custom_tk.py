2d03fe0c0d01344b0ca5ed146d072055e03b428a
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2025-03-13 03:11:20 +0000

    Rename custom_ttk to custom_ttk

---
+++ _helpers/custom_tk.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class Limiter(ttk.Scale):
+    """ ttk.Scale sublass that limits the precision of values. """
+
+    def __init__(self, *args, **kwargs):
+        self.precision = kwargs.pop('precision')  # Remove non-std kwarg.
+        self.chain = kwargs.pop('command', lambda *a: None)  # Save if present.
+        super(Limiter, self).__init__(*args, command=self._value_changed, **kwargs)
+
+    def _value_changed(self, newvalue):
+        newvalue = round(float(newvalue), self.precision)
+        if self.precision == 0:
+            newvalue = int(newvalue)
+        self.winfo_toplevel().globalsetvar(self.cget('variable'), (newvalue))
+        self.chain(newvalue)  # Call user specified function.
+
+
+class ReadOnlyTextWithVar(tk.Frame):
+    def __init__(self, parent, textvariable=None, **kwargs):
+        super().__init__(parent)
+
+        self.text_widget = tk.Text(self, **kwargs, state="disabled")
+        self.text_widget.pack(expand=True, fill="both")
+
+        self.textvariable = textvariable
+        self.yview = self.text_widget.yview
+
+        # Initialize text from textvariable
+        if self.textvariable is not None:
+            self.update_text_widget()
+            self.textvariable.trace_add("write", lambda *_: self.update_text_widget())
+
+    def update_text_widget(self):
+        """Update the Text widget when the StringVar changes."""
+        if self.textvariable is not None:
+            new_text = self.textvariable.get()
+            
+            # Enable text widget temporarily to modify text
+            self.text_widget.config(state="normal")
+            self.text_widget.delete("1.0", tk.END)
+            self.text_widget.insert("1.0", new_text)
+            self.text_widget.config(state="disabled")  # Re-disable input

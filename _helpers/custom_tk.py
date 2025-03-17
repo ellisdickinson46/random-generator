302a817bf798eddce42ead94bd320127af6f85ea
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2025-03-17 17:59:12 +0000

    Add and remove functions

--- _helpers/custom_tk.py
+++ _helpers/custom_tk.py
@@ -75,25 +75,39 @@ class ScrollableListbox(tk.Frame):
         if header:
             show = "tree headings"
 
-        self._treeview = ttk.Treeview(self, show=show, **kwargs)
-        self._scrollbar = ttk.Scrollbar(self, command=self._treeview.yview)
-        self._treeview.configure(yscrollcommand=self._scrollbar.set)
+        self.treeview = ttk.Treeview(self, show=show, **kwargs)
+        self.scrollbar = ttk.Scrollbar(self, command=self.treeview.yview)
+        self.treeview.configure(yscrollcommand=self.scrollbar.set)
 
-        self._treeview.pack(side="left", fill="both", expand=True)
-        self._scrollbar.pack(side="right", fill="y")
+        self.treeview.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
 
-        self._treeview.column("#0", width=0)
-        self._treeview.heading('#0', text=header)
+        self.treeview.column("#0", width=0)
+        self.treeview.heading('#0', text=header)
     
     def add_item(self, item):
-        self._treeview.insert("", "end", text=item)
+        if item:
+            # Get all existing items in the treeview
+            existing_items = [self.treeview.item(child, 'text') for child in self.treeview.get_children()]
+            
+            if item not in existing_items:
+                print(f"Adding: {item}")
+                # Capture the ID of the newly inserted item
+                new_item_id = self.treeview.insert("", "end", text=item)
+                
+                # Select the newly inserted item
+                self.treeview.selection_set(new_item_id)
+                self.treeview.focus(new_item_id)  # Optional: Set focus to the new item
+                self.treeview.yview_moveto(1.0)
+            else:
+                print(f"Item '{item}' already exists.")
 
     def rem_item(self):
-        selection = self._treeview.selection()
+        selection = self.treeview.selection()
         for item in selection:
-            self._treeview.delete(item)
+            self.treeview.delete(item)
     
     def add_sample_items(self):
         for i in range(100):
             text = f"Item #{i + 1}"
-            self._treeview.insert("", "end", text=text)
+            self.treeview.insert("", "end", text=text)

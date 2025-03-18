94e6c2aee71328eeb7c59cd47fe5fcb6697a3ba9
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2025-03-18 04:04:26 +0000

    Add is_hex_color()

--- _helpers/validate.py
+++ _helpers/validate.py
@@ -1,3 +1,4 @@
+import re
 
 def is_integer(value):
     return value.isdigit()
@@ -6,3 +7,7 @@ def is_in_list(values_list, value):
     if value in values_list:
         return True
     return False
+
+def is_hex_color(code):
+    pattern = r'^#([0-9A-Fa-f]{6})$'
+    return bool(re.match(pattern, code))

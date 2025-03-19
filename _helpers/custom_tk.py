a668fb78f61ac132341bc4e091acc38c2995c4bc
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2025-03-19 06:10:43 +0000

    Implement a tkinter ListVar for callback support

--- _helpers/custom_tk.py
+++ _helpers/custom_tk.py
@@ -147,3 +147,55 @@ class ScrollableListbox(tk.Frame):
         for i in range(100):
             text = f"Item #{i + 1}"
             self.treeview.insert("", "end", text=text)
+
+
+class ListVar(tk.Variable):
+    def __init__(self, master=None, value=None, name=None):
+        super().__init__(master, name)
+        if value is None:
+            value = []
+        self.set(value)
+    
+    def get(self):
+        # Convert stored string to list
+        value = super().get()
+        if isinstance(value, str):
+            # Handle the special case for an empty string
+            if value == '':
+                return []
+            return value.split(' ')
+        return value
+    
+    def set(self, value):
+        if isinstance(value, list):
+            if not value:
+                # Store an empty string for an empty list
+                value = ''
+            else:
+                # Use list comprehension for better clarity
+                value = ' '.join([str(item) for item in value])
+        super().set(value)
+    
+    def append(self, item):
+        current = self.get()
+        current.append(item)
+        self.set(current)
+    
+    def remove(self, item):
+        current = self.get()
+        if item in current:
+            current.remove(item)
+            self.set(current)
+    
+    def clear(self):
+        self.set([])
+    
+    def pop(self, index=-1):
+        current = self.get()
+        if current:
+            item = current.pop(index)
+            self.set(current)
+            return item
+    
+    def __repr__(self):
+        return repr(self.get())
